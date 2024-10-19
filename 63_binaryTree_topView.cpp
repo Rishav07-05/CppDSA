b92c539d60df09ebeca9b1869014e37f1899b972
Rishav Kumar <rishavkumar18902@gmail.com>
2024-10-19 14:36:29 +0530

    Top view of tree

---
+++ 63_binaryTree_topView.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+    node(int d)
+    {
+        this->data = d;
+        this->left = NULL;
+        this->right = nullptr;
+    }
+};
+
+node *buildTree(node *root)
+{
+    cout << "Enter the data: " << endl;
+    int data;
+    cin >> data;
+    root = new node(data);
+    if (data == -1)
+    {
+        return nullptr;
+    }
+    cout << "Enter data to insert at left: " << data << endl;
+    root->left = buildTree(root->left);
+    cout << "Enter data to insert at right: " << data << endl;
+    root->right = buildTree(root->right);
+    return root;
+}
+
+vector<int> topView(node *root){
+    vector<int> ans;
+    if(root == NULL)
+        return ans;
+
+    map<int, int> topNode;
+    queue<pair<node *, int>> q;
+
+    q.push(make_pair(root, 0));
+    while(!q.empty()){
+        pair<node *, int> temp = q.front();
+        q.pop();
+        node *frontNode = temp.first;
+        int hd = temp.second;
+
+        if(topNode.find(hd) == topNode.end())
+            topNode[hd] = frontNode->data;
+
+        if(frontNode -> left){
+            q.push(make_pair(frontNode->left, hd - 1));
+        }
+        if(frontNode -> right){
+            q.push(make_pair(frontNode->right, hd + 1));
+        }
+    }
+    for(auto i: topNode){
+        ans.push_back(i.second);
+    }
+    return ans;
+}
+
+int main()
+{
+    node *root = nullptr;
+    root = buildTree(root);
+    vector<int> ans = topView(root);
+    cout << "Top View: ";
+    for(auto i : ans){
+        cout << i << " ";
+    }
+    cout << endl;
+}
