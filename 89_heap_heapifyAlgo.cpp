a38bb617b3622df12e11397e08658b0a26d7367e
Rishav Kumar <rishavkumar18902@gmail.com>
2024-11-01 19:54:14 +0530

    Heapify Algo

---
+++ 89_heap_heapifyAlgo.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+class heap{
+    public:
+        int arr[100];
+        int size;
+    heap(){
+        arr[0] = -1;
+        size = 0;
+    }
+        void insert(int val){
+            size += 1;
+            int index = size;
+            arr[index] = val;
+            while(index > 1){
+                int parent = index / 2;
+                if(arr[parent] < arr[index]){
+                    swap(arr[index], arr[parent]);
+                    index = parent;
+                }
+                else{
+                    return;
+                }
+            }
+        }
+
+        void print(){
+            for (int i = 1; i <= size; i++){
+                cout << arr[i] << " ";
+            }
+            cout << endl;
+        }
+};
+
+
+void heapify(int arr[] , int n , int i){
+    int largest = i;
+    int left = 2 * i;
+    int right = 2 * i + 1;
+
+    if(left < n && arr[largest] < arr[left]){
+        largest = left;
+    }
+    if(right < n && arr[largest] < arr[right]){
+        largest = right;
+    }
+    
+    if(largest != i){
+        swap(arr[largest], arr[i]);
+        heapify(arr, n, largest);
+    }
+}
+
+int main(){
+    heap h;
+    h.insert(50);
+    h.insert(55);
+    h.insert(53);
+    h.insert(52);
+    h.insert(54);
+    h.print();
+
+    int arr[6] = {-1, 54, 53, 55, 52, 50};
+    int n = 5;
+    for (int i = n / 2; i > 0; i--){
+        heapify(arr, n, i);
+    }
+    for (int i = 1; i <= n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
